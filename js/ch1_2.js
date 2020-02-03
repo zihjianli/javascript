72ba80baa8432682fd9b6ac83337fb85980425c5
zihjianli <vbaic123@gmail.com>
2020-02-03 10:49:32 +0800

    20200203

---
+++ js/ch1_2.js
@@ -0,0 +1,13 @@
+window.onload = function(){  
+  alert(0);
+  alert(1);
+  alert(2);
+  alert(3);
+  alert(4);
+}
+
+// alert(0);
+// alert(1);
+// alert(2);
+// alert(3);
+// alert(4);
---
+++ js/ch1_3.js
@@ -0,0 +1,6 @@
+window.onload = function(){  
+  console.log("我出現在畫面上了"); 
+  console.log(0); 
+  console.log(1);
+  console.error("發生錯誤了");
+}
